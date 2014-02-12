9a077b0bb78537850a37a1f943e1294c41c5ea20
Simon Timms <stimms@gmail.com>
2014-02-12 07:08:31 -0700

    Initial add

---
+++ TestApplication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string sessionValue)
+        {
+            if (sessionValue != null)
+                Session.Add("testValue", sessionValue);
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
